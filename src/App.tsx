804a3a651c819cbf705ca2092bdd983fbf78fe8e
Jakub Bedynek <kuba.xray@gmail.com>
2023-03-28 18:11:34 +0200

    Added routing and views

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import { Login } from "./View/Login";
+import { Main } from "./View/Main";
+import { Playlist } from "./View/Playlist";
+import { Search } from "./View/Search";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Main />}></Route>
+            <Route path="/Login" element={<Login />}></Route>
+            <Route path="/Playlist" element={<Playlist />}></Route>
+            <Route path="/Search" element={<Search />}></Route>
+          </Routes>
+        </BrowserRouter>
       </header>
     </div>
   );
---
+++ src/View/Playlist.tsx
@@ -0,0 +1,10 @@
+import { BackButton } from "../components/BackButton/BackButton";
+
+export const Playlist = () => {
+  return (
+    <>
+      <h1>Playlist Page</h1>
+      <BackButton />
+    </>
+  );
+};
